aecef5ef510abbbad7060bca626014f80231e7f8
Bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2023-06-12 10:08:56 -0500

    fixup! [script] add indexer supported commit hash

--- scripts/convert_prowlarr_supported_indexers_to_markdown.py
+++ scripts/convert_prowlarr_supported_indexers_to_markdown.py
@@ -448,7 +448,7 @@ def main(app_commit, indexer_commit, build, app_apikey, output_file, app_base_ur
     # Page Header Info
     wiki_page_start = (
         WIKI_1NEWLINE
-        + "- Supported Trackers and Indexers as of "
+        + "- Supported Trackers and Indexers as of"
         + WIKI_1NEWLINE
         + f"  - Prowlarr Build `{build}` / [Commit: {app_commit}]({GH_APP_COMMIT_URL.rstrip('/')}/{app_commit})"
         + WIKI_1NEWLINE
