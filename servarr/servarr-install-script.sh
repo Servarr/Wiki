ced808c0fb8c556702cf055f7768f0419616368b
The Nostrus Dominion <admin@nostrus-dominion.co>
2024-04-10 16:26:32 -0400

    Updates to install/uninstall scripts. (#349)
    * Updated script.
    
    Added title splash, added colors, attempting to improve readability, check for installed prerequisites, supressed tarball extraction, and added some sleep timers.
    
    * Updated script.
    
    I moved to check as root before the title splash.
    
    * Updated install/uninstall scripts
    
    Check version history in the scripts to see a more descriptive (HA!) list of what was changed.
    
    * Update servarr/servarr-install-script.sh
    
    Co-authored-by: bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
    
    * Updated install/uninstall scripts
    
    Per https://github.com/bakerboy448 request
    
    * Updated install/uninstall scripts.
    
    I fucked something up but I fixed it.
    
    ---------
    
    Co-authored-by: bakerboy448 <55419169+bakerboy448@users.noreply.github.com>

--- servarr/servarr-install-script.sh
+++ servarr/servarr-install-script.sh
@@ -18,6 +18,7 @@
 ### Version v3.0.9a 2023-07-14 - DoctorArr - updated scriptversion and scriptdate and to see how this is going! It was still at v3.0.8.
 ### Version v3.0.10 2024-01-04 - Bakerboy448 - Misc updates and refactoring. Move to own script file.
 ### Version v3.0.11 2024-01-06 - StevieTV - Exit script when ran from installdir
+### Version v3.0.12 2024-04-09 - nostrus-dominion - moved root check, added title splash, added colors, attempted to improve readability, check for installed prerequisites before bothering apt, supressed tarball extraction, added some sleep timers.
 ### Additional Updates by: The Servarr Community
 
 ### Boilerplate Warning
@@ -29,22 +30,44 @@
 #OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 #WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-scriptversion="3.0.11"
-scriptdate="2024-01-06"
+### Colors
+green='\033[0;32m'
+yellow='\033[1;33m'
+red='\033[0;31m'
+brown='\033[0;33m'
+reset='\033[0m' # No Color
+
+scriptversion="3.0.12"
+scriptdate="2024-04-10" # change this later
 
 set -euo pipefail
 
-echo "Running Servarr Install Script - Version [$scriptversion] as of [$scriptdate]"
-
-# Am I root?, need root!
+### Am I root?, need root! GROOT!
 
 if [ "$EUID" -ne 0 ]; then
-    echo "Please run as root."
+    echo -e ${red}"Please run as root!"
+    echo -e "Exiting script!"
     exit
 fi
 
-echo "Select the application to install: "
+### Title Splash
+echo -e ${brown}
+echo -e "#############################################################"
+echo -e "#                                                           #"
+echo -e "#   Welcome to the Servarr Community Installation Script!   #"
+echo -e "#                                                           #"
+echo -e "#  This script is for any Debian-based distro users         #"
+echo -e "#  who need a little help with just the press of a button.  #"
+echo -e "#  If you have not done so, exit the script and read the    #"
+echo -e "#  Boilerplate Warning just to CYA. Enjoy your new setup!   #"
+echo -e "#                                                           #"
+echo -e "#############################################################"
+echo -e ${reset}
 
+echo "Running Servarr Install Script - Version ${brown}[$scriptversion]${reset} as of ${brown}[$scriptdate]${reset}"
+echo ""
+echo "Select the application to install: "
+echo ""
 select app in lidarr prowlarr radarr readarr whisparr quit; do
 
     case $app in
@@ -91,8 +114,9 @@ select app in lidarr prowlarr radarr readarr whisparr quit; do
         ;;
     esac
 done
+echo ""
 
-# Constants
+### CONSTANTS
 ### Update these variables as required for your specific instance
 installdir="/opt"              # {Update me if needed} Install Location
 bindir="${installdir}/${app^}" # Full Path to Install Location
@@ -100,32 +124,58 @@ datadir="/var/lib/$app/"       # {Update me if needed} AppData directory to use
 app_bin=${app^}                # Binary Name of the app
 
 if [[ $app != 'prowlarr' ]]; then
-    echo "It is critical that the user and group you select to run ${app^} as will have READ and WRITE access to your Media Library and Download Client Completed Folders"
+    echo -e ${red}"   WARNING! WARNING! WARNING!"${reset}
+    echo ""
+    echo -e "   It is ${red}CRITICAL${reset} that the ${brown}User${reset} and ${brown}Group${reset} you select"
+    echo -e "   to run ${brown}[${app^}]${reset} will have both ${red}READ${reset} and ${red}WRITE${reset} access"
+    echo -e "   to your Media Library and Download Client directories!"${reset}
+    sleep 5
 fi
 
 # This script should not be ran from installdir, otherwise later in the script the extracted files will be removed before they can be moved to installdir.
 if [ "$installdir" == "$(dirname -- "$( readlink -f -- "$0"; )")" ] || [ "$bindir" == "$(dirname -- "$( readlink -f -- "$0"; )")" ]; then
-    echo "You should not run this script from the intended install directory. The script will exit. Please re-run it from another directory"
+    echo ""
+    echo -e "${red}Error!${reset} You should not run this script from the intended install directory."
+    echo "Please re-run it from another directory."
+    echo "Exiting Script!"
     exit
 fi
 
 # Prompt User
-read -r -p "What user should ${app^} run as? (Default: $app): " app_uid
+echo ""
+read -r -p "What user should [${app^}] run as? (Default: $app): " app_uid
 app_uid=$(echo "$app_uid" | tr -d ' ')
 app_uid=${app_uid:-$app}
 # Prompt Group
-read -r -p "What group should ${app^} run as? (Default: media): " app_guid
+echo ""
+read -r -p "What group should [${app^}] run as? (Default: media): " app_guid
 app_guid=$(echo "$app_guid" | tr -d ' ')
 app_guid=${app_guid:-media}
 
-echo "${app^} selected"
-echo "This will install [${app^}] to [$bindir] and use [$datadir] for the AppData Directory"
+echo ""
+echo -e "${brown}[${app^}]${reset} selected for installation."
+echo ""
+echo -e "${brown}[${app^}]${reset} will then be installed to ${brown}[$bindir]${reset} and use ${brown}[$datadir]${reset} for the AppData Directory."
 if [[ $app == 'prowlarr' ]]; then
-    echo "${app^} will run as the user [$app_uid] and group [$app_guid]."
+    echo ""
+    echo -e "${brown}[${app^}]${reset} will run as the user ${brown}[$app_uid]${reset} and group ${brown}[$app_guid]${reset}."
 else
-    echo "${app^} will run as the user [$app_uid] and group [$app_guid]. By continuing, you've confirmed that that user and group will have READ and WRITE access to your Media Library and Download Client Completed Download directories"
+    echo ""
+    echo -e "${brown}[${app^}]${reset} will run as the user ${brown}[$app_uid]${reset} and group ${brown}[$app_guid]${reset}."
+    echo ""
+    echo -e "   By continuing, you've ${red}CONFIRMED${reset} that that ${brown}[$app_uid]${reset} and ${brown}[$app_guid]${reset}"
+    echo -e "   will have both ${red}READ${reset} and ${red}WRITE${reset} access to all required directories."
+    echo ""
 fi
-read -n 1 -r -s -p $'Press enter to continue or ctrl+c to exit...\n'
+
+# User confirmation that installation will continue
+echo ""
+read -r -p "Please type 'yes' to continue with the installation: " response
+if [[ $response != "yes" && $response != "YES" ]]; then
+    echo "Invalid response. Operation is canceled!"
+    echo "Exiting script!"
+    exit 0
+fi
 
 # Create User / Group as needed
 if [ "$app_guid" != "$app_uid" ]; then
@@ -135,35 +185,60 @@ if [ "$app_guid" != "$app_uid" ]; then
 fi
 if ! getent passwd "$app_uid" >/dev/null; then
     adduser --system --no-create-home --ingroup "$app_guid" "$app_uid"
-    echo "Created and added User [$app_uid] to Group [$app_guid]"
+    echo ""
+    echo -e "Created User ${yellow}$app_uid${reset}"
+    echo ""
+    echo -e "Created Group ${yellow}$app_guid${reset}."
+    sleep 3
 fi
 if ! getent group "$app_guid" | grep -qw "$app_uid"; then
-    echo "User [$app_uid] did not exist in Group [$app_guid]"
+    echo ""
+    echo -e "User ${yellow}$app_uid${reset} did not exist in Group ${yellow}$app_guid${reset}."
     usermod -a -G "$app_guid" "$app_uid"
-    echo "Added User [$app_uid] to Group [$app_guid]"
+    echo ""
+    echo -e "Added User ${yellow}$app_uid${reset} to Group ${yellow}$app_guid${reset}."
+    sleep 3
 fi
 
 # Stop the App if running
 if service --status-all | grep -Fq "$app"; then
     systemctl stop "$app"
     systemctl disable "$app".service
-    echo "Stopped existing $app"
+    echo "Stopped existing $app."
 fi
 
-# Create Appdata Directory
-
-# AppData
+# Create Appdata Directories
 mkdir -p "$datadir"
 chown -R "$app_uid":"$app_guid" "$datadir"
 chmod 775 "$datadir"
-echo "Directories created"
+echo ""
+echo -e "Directories ${yellow}$bindir${reset} and ${yellow}$datadir${reset} created!"
+
 # Download and install the App
 
-# prerequisite packages
+# Check if prerequisite packages are already installed and install them if needed
 echo ""
-echo "Installing pre-requisite Packages"
-# shellcheck disable=SC2086
-apt update && apt install $app_prereq
+echo -e ${yellow}"Checking Pre-Requisite Packages..."${reset}
+sleep 3
+
+missing_packages=()
+for pkg in $app_prereq; do
+    if ! dpkg -s "$pkg" >/dev/null 2>&1; then
+        missing_packages+=("$pkg")
+    fi
+done
+
+if [ ${#missing_packages[@]} -eq 0 ]; then
+    echo ""
+    echo -e ${green}"All prerequisite packages are already installed!"${reset}
+else
+    echo ""
+    echo -e "Installing missing prerequisite packages: ${brown}${missing_packages[*]}${reset}"
+    # Install missing prerequisite packages
+    apt update && apt install "${missing_packages[@]}"
+fi
+
+# check if architecture is correct
 echo ""
 ARCH=$(dpkg --print-architecture)
 # get arch
@@ -173,41 +248,58 @@ case "$ARCH" in
 "armhf") DLURL="${dlbase}&arch=arm" ;;
 "arm64") DLURL="${dlbase}&arch=arm64" ;;
 *)
-    echo "Arch not supported"
+    echo -e ${red}"Your arch is not supported!"
+    echo -e "Exiting installer script!"${reset}
     exit 1
     ;;
 esac
-echo ""
-echo "Removing previous tarballs"
+
+echo -e ${yellow}"Removing tarballs..."${reset}
+sleep 3
 # -f to Force so we do not fail if it doesn't exist
 rm -f "${app^}".*.tar.gz
 echo ""
-echo "Downloading..."
-wget --content-disposition "$DLURL"
-tar -xvzf "${app^}".*.tar.gz
+echo -e ${yellow}"Downloading required files..."${reset}
 echo ""
-echo "Installation files downloaded and extracted"
+wget --content-disposition "$DLURL"
+echo ""
+echo -e ${yellow}"Download complete!"${reset}
+echo ""
+echo -e ${yellow}"Extracting tarball!"${reset}
+tar -xvzf "${app^}".*.tar.gz >/dev/null 2>&1
+echo ""
+echo -e ${yellow}"Installation files downloaded and extracted!"${reset}
 
 # remove existing installs
-echo "Removing existing installation"
+echo ""
+echo -e "Removing existing installation files from ${brown}[$bindir]"${reset}
 rm -rf "$bindir"
-echo "Installing..."
+sleep 2
+echo ""
+echo -e "Attempting to install ${brown}[${app^}]${reset}..."
+sleep 2
 mv "${app^}" $installdir
 chown "$app_uid":"$app_guid" -R "$bindir"
 chmod 775 "$bindir"
-rm -rf "${app^}.*.tar.gz"
 # Ensure we check for an update in case user installs older version or different branch
 touch "$datadir"/update_required
 chown "$app_uid":"$app_guid" "$datadir"/update_required
-echo "App Installed"
+echo ""
+echo -e "Successfully installed ${brown}[${app^}]${reset}!!"
+rm -rf "${app^}.*.tar.gz"
+sleep 2
+
 # Configure Autostart
 
 # Remove any previous app .service
-echo "Removing old service file"
+echo ""
+echo "Removing old service file..."
 rm -rf /etc/systemd/system/"$app".service
+sleep 2
 
 # Create app .service with correct user startup
-echo "Creating service file"
+echo ""
+echo "Creating new service file..."
 cat <<EOF | tee /etc/systemd/system/"$app".service >/dev/null
 [Unit]
 Description=${app^} Daemon
@@ -224,23 +316,50 @@ Restart=on-failure
 [Install]
 WantedBy=multi-user.target
 EOF
+sleep 2
 
 # Start the App
-echo "Service file created. Attempting to start the app"
+echo ""
+echo -e "New service file created!"
+echo ""
+echo -e "${brown}[${app^}]${reset} is attempting to start, this may take a few seconds..."
 systemctl -q daemon-reload
 systemctl enable --now -q "$app"
+sleep 3
 
-# Finish Update/Installation
+# Check if the service is up and running
+echo ""
+echo "Checking if the service is up and running..."
+
+# Loop to wait until the service is active
+while ! systemctl is-active --quiet "$app"; do
+    sleep 1
+done
+
+echo ""
+echo -e "${brown}[${app^}]${reset} installation and service start up is complete!"
+
+# Finish Installation
 host=$(hostname -I)
 ip_local=$(grep -oP '^\S*' <<<"$host")
 echo ""
-echo "Install complete"
-sleep 10
+echo -e "Attempting to check for a connection at http://$ip_local:$app_port..."
+sleep 3
 STATUS="$(systemctl is-active "$app")"
 if [ "${STATUS}" = "active" ]; then
-    echo "Browse to http://$ip_local:$app_port for the ${app^} GUI"
+    echo ""
+    echo "Successful connection!"
+    echo ""
+    echo -e "Browse to ${green}http://$ip_local:$app_port${reset} for the GUI."
+    echo ""
+    echo "Script complete! Exiting now!"
+    echo ""
 else
-    echo "${app^} failed to start"
+    echo ""
+    echo -e ${red}"${app^} failed to start."${reset}
+    echo ""
+    echo "Please try again. Exiting script."
+    echo
 fi
 
 # Exit
--- servarr/servarr-uninstall-script.sh
+++ servarr/servarr-uninstall-script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+### Version 0.0.2 - nostrusdominion - moved root check, added colors, improved reability, added check for what services are installed instead of just listing services, 1removed AppData confirmation it felt redundant, added confirmations for user and group removal, added a check for users in $group before deletion,
+
 ### Boilerplate Warning
 #THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 #EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
@@ -9,45 +11,125 @@
 #OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 #WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-scriptversion="0.0.1"
-scriptdate="2024-04-01"
+### Colors
+green='\033[0;32m'
+yellow='\033[1;33m'
+red='\033[0;31m'
+brown='\033[0;33m'
+reset='\033[0m' # No Color
+
+scriptversion="0.0.2"
+scriptdate="2024-04-10"
 
 set -euo pipefail
-
-echo "Running Servarr Uninstall Script - Version [$scriptversion] as of [$scriptdate]"
 
 # Am I root?, need root!
 
 if [ "$EUID" -ne 0 ]; then
-    echo "Please run as root."
+    echo -e ${red}"Please run as root!"
+    echo -e "Exiting script!"
     exit
 fi
 
-#!/bin/bash
+### Title Splash
+echo -e ${brown}
+echo -e "   #############################################################"
+echo -e "   #                                                           #"
+echo -e "   #   Welcome to the Servarr Community UNInstaller Script!    #"
+echo -e "   #                                                           #"
+echo -e "   #  This script is for any Debian distro based users         #"
+echo -e "   #  who need a little help with just the press of a button.  #"
+echo -e "   #  If you have not done so, exit the script and read the    #"
+echo -e "   #  Boilerplate Warning just to CYA. Enjoy your new setup!   #"
+echo -e "   #                                                           #"
+echo -e "   #############################################################"
+echo -e ${reset}
 
-# Default application name; override by enter manually
-select app in "lidarr" "prowlarr" "radarr" "readarr" "whisparr" "manual" "quit "; do
+echo -e "Running Servarr Uninstall Script - Version ${brown}[$scriptversion]${reset} as of ${brown}[$scriptdate]${reset}"
+echo ""
 
+### FIND THE SERVICES
+
+# Function to check if a service is running
+service_is_running() {
+    systemctl is-active --quiet "$1"
+}
+
+# Array of services
+services=("lidarr" "prowlarr" "radarr" "readarr" "whisparr")
+
+### Fake echo to let the user think the computer is searching
+
+# Function to display the ellipsis
+display_ellipsis() {
+    local count=0
+    local direction=1
+    local max_count=3
+
+    # Loop for 7 seconds
+    for ((i = 0; i < 7; i++)); do
+        # Print the ellipsis
+        echo -ne "Scanning for running services"
+        for ((j = 0; j < count; j++)); do
+            echo -n "."
+        done
+        echo -ne "\r"
+
+        # Increment or decrement the count based on direction
+        count=$((count + direction))
+
+        # Change direction if count reaches max or min
+        if [ "$count" -eq "$max_count" ] || [ "$count" -eq 0 ]; then
+            direction=$((direction * -1))
+        fi
+
+        sleep 1  # Wait for 1 second
+    done
+    echo ""  # Print a newline after the loop
+}
+
+display_ellipsis
+
+# List of running services
+running_services=()
+for service in "${services[@]}"; do
+    if service_is_running "$service"; then
+        running_services+=("$service")
+    fi
+done
+
+# Add "manual" and "quit" options
+running_services+=("manual" "quit")
+
+# Prompt user to select an option
+echo ""
+echo "Select an application:"
+select app in "${running_services[@]}"; do
     case $app in
         manual)
-                read -p "Enter the service name manually: " manual_app
-                app="$manual_app"
-                break
-                ;;
+            read -p "Enter the service to remove: " manual_app
+            app="$manual_app"
+            break
+            ;;
         quit)
             exit 0
             ;;
         *)
-        echo "You selected: $app"
-        break
-        esac
-    done
+            echo ""
+            echo -e "You selected: ${brown}${app}${reset}"
+            break
+            ;;
+    esac
+done
+
 
 # Extract paths and user/group from systemd service file
 ExecStart=$(systemctl show -p ExecStart --value "$app.service")
 
 if [ -z "$ExecStart" ]; then
-    echo "Error: Failed to find matching service name. Aborting."
+    echo ""
+    echo -e "${red}Error${reset}: Failed to find matching service name. Aborting."
+    echo ""
     exit 1
 fi
 
@@ -56,42 +138,108 @@ Group=$(systemctl show -p Group --value "$app.service")
 bindir=$(echo "$ExecStart" | awk -F ' ' '{ for(i=1; i<=NF; i++) { if ($i ~ /path=/) { split($i, path, "="); print path[2]; } } }')
 appdatadir=$(echo "$ExecStart" | awk -F ' ' '{ for(i=1; i<=NF; i++) { if ($i ~ /-data=/) { split($i, data, "="); print data[2]; } } }')
 
-read -e -p "Found data directory to be '$appdatadir'. Do you want to change this? (press Enter to accept): " userdatadir
+# Output all relevant data
+echo ""
+echo -e "Installation directory: ${brown}$bindir${reset}"
+echo ""
+echo -e "AppData directory: ${brown}$appdatadir${reset}"
+echo ""
+echo -e "Service is running as User: ${brown}$User${reset}"
+echo ""
+echo -e "Service is running in Group: ${brown}$Group${reset}"
 
-# If variable is empty, use parsed value
-if [ -n "$userdatadir" ]; then
-    appdatadir=$userdatadir
+# User confirmation that uninstallation will continue
+echo ""
+read -r -p "Please type 'yes' to continue with uninstallation: " response
+if [[ $response != "yes" && $response != "YES" ]]; then
+    echo "Invalid response. Uninstallation canceled."
+    echo "Exiting script!"
+    exit 0
 fi
 
-echo "Installation directory (bindir): $bindir"
-echo "Data directory (appdatadir): $appdatadir"
-echo "Running as User: $User, Group: $Group"
-
-# Instructions for user and group removal
-echo "To remove the user and/or group, use: sudo userdel $User; sudo groupdel $Group"
-
-# Proceed with uninstallation as before
-read -n 1 -r -s -p $'Press enter to continue or ctrl+c to exit...\n'
+# Proceed with uninstallation
+echo ""
+echo -e ${yellow}"Uninstallation in progress!"${reset}
+sleep 1
 
 # Stop and disable the service
-systemctl stop "$app"
-systemctl disable "$app.service"
-echo "${app^} service stopped and disabled."
+echo ""
+echo -e "Attempting to stop and disable services..."
+sleep 2
+systemctl stop "$app"  >/dev/null 2>&1
+systemctl disable "$app.service"  >/dev/null 2>&1
+echo ""
+echo -e "${brown}[${app^}]${reset} service has been stopped and disabled."
+sleep 2
 
 # Remove the service file
 rm -f /etc/systemd/system/"$app".service
 systemctl daemon-reload
-echo "Service file removed."
+echo ""
+echo -e ${yellow}"Service file removed from systemd."${reset}
+sleep 2
 
 # Prompt for data directory deletion
+echo ""
 read -p "Do you want to delete the data directory at '$appdatadir'? (y/N): " deldata
+deluser=${deluser:-N}
+
 if [[ $deldata =~ ^[Yy]$ ]]; then
     rm -rf "$appdatadir"
-    echo "Data directory deleted."
+    echo ""
+    echo -e ${yellow}"Data directory deleted."${reset}
+    sleep 2
+fi
+
+# User confirmation remove $User
+echo ""
+read -p "Do you want to delete USER '$User'? (y/N): " deluser
+deluser=${deluser:-N}
+
+if [[ $deluser =~ ^[Yy]$ ]]; then
+    sudo userdel $User
+    echo ""
+    echo "User '$User' has been removed from your system."
+    sleep 2
+else
+    echo ""
+    echo -e ${yellow}"User '$User' has not to be removed from your system."${reset}
+    sleep 2
+fi
+
+# Check if there are users in the group
+users_in_group=$(getent group "$Group" | cut -d: -f4)
+if [ -n "$users_in_group" ]; then
+    echo ""
+    echo -e "There are still users in the group '$Group':" ${yellow}$users_in_group${reset}
+    sleep 1
+    echo ""
+    echo -e "Group '$Group' ${red}CANNOT${reset} be removed until all users are removed from it."
+    echo ""
+    echo "If this user is no longer in use, then as root use 'userdel $users_in_group' to delete the user from your system."
+    sleep 1
+else
+    echo ""
+    read -p "Do you want to delete GROUP '$Group'? (y/N): " delgroup
+    delgroup=${delgroup:-N}
+
+    if [[ $delgroup =~ ^[Yy]$ ]]; then
+        sudo groupdel "$Group"
+        echo ""
+        echo "Group '$Group' has been removed from your system."
+    else
+        echo ""
+        echo "Group '$Group' will not be removed from your system."
+    fi
 fi
 
 # Remove the application directory
 rm -rf "$bindir"
-echo "${app^} installation directory removed."
+echo ""
+echo -e "${brown}${app^}${reset} installation directory removed from $bindir."
+echo ""
+echo -e "${brown}${app^}${reset} uninstallation has been completed."
+echo ""
+echo "Script is complete. Exiting NOW!"
 
-echo "${app^} uninstallation completed."
+exit
